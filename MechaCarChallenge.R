ea9555c4b9f788fb984fe65b4036be64a7d4ed58
Carlos Morales <carlosmorales@Carloss-MacBook-Pro-2.local>
2022-09-06 14:49:42 -0700

    adding deliverable 2 in RScript

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -13,3 +13,18 @@ lm(mpg ~ vehicle_length + vehicle_length + spoiler_angle + ground_clearance + AW
 
 #generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data =MechaCar_mpg_df)) 
+
+
+#Deliverable 2: Create Visualizations for the Trip Analysis 
+
+#import and read csv file as data frame
+Suspension_Coil_df <- read.csv(file='Suspension_Coil.csv', check.names = F,stringsAsFactors = F)
+
+#mean, median, variance, and standard deviation of the suspension coil’s PSI column.
+Mean = mean(Suspension_Coil_df$PSI)
+Median=median(Suspension_Coil_df$PSI)
+Variance=var(Suspension_Coil_df$PSI)
+SD = sd(Suspension_Coil_df$PSI)
+
+#Total_Summary Data Frame
+total_Summary <- data.frame(Mean,Median,Variance,SD)
